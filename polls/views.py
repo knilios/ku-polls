634f5e5ca339cf0d053b0d7df6800e58fa335d56
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-09-01 22:47:09 +0700

    Update test_future_question, added docstings and clean up the files

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,9 @@ from django.contrib import messages
 
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
+    """This class handles the index page"""
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
@@ -19,47 +21,57 @@ class IndexView(generic.ListView):
             (not including those set to be published in the future)
         """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-    
-    
+
+
 class DetailView(generic.DetailView):
+    """This class handles the detail page"""
     model = Question
     template_name = "polls/detail.html"
-    
+
     def get_queryset(self):
         """
         Excludes any question that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
-    
+
     def dispatch(self, request, *args, **kwargs) -> HttpResponse:
+        """
+        This method redirects the user if the polls does not exists.
+        """
         try:
             return super().dispatch(request, *args, **kwargs)
         except Http404:
             return redirect(reverse("polls:index"))
-    
+
 
 class ResultsView(generic.DetailView):
+    """This class handles the detail page"""
     model = Question
     template_name = "polls/results.html"
-    
+
     def dispatch(self, request, *args, **kwargs) -> HttpResponse:
+        """
+        This method redirects the user if the polls does not exists.
+        """
         try:
             return super().dispatch(request, *args, **kwargs)
         except Http404:
             return redirect(reverse("polls:index"))
-    
+
 
 def vote(request, question_id):
+    """This function handles the POST request from the using voting on the poll"""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except Exception:
-        messages.error(request, "You didn't select a choice. Please consider doing so.")
+        messages.error(
+            request, "You didn't select a choice. Please consider doing so.")
         return render(
             request,
             "polls/detail.html",
             {
-                "question":question
+                "question": question
             },
         )
     else:
@@ -67,4 +79,3 @@ def vote(request, question_id):
         selected_choice.save()
         # Always return a redirect after a POST request. :D
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    
