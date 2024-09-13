4bad42614bec988df7decec44e87a86221ab47b2
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-09-13 17:51:23 +0700

    Added installation

--- installation/linux-macos.sh
+++ installation/linux-macos.sh
@@ -1,8 +1,8 @@
 @echo off
-echo Installing virtual environment
+echo Installing vertual environment
 py -3.11 -m venv venv
-if %errorlevel% equ 0 (
-    call venv/Scripts/activate
+if [ $? -eq 0] then
+    source venv/bin/activate
     echo Installing python requirements
     python -m pip install -r requirements.txt
     echo Initializing Django
@@ -10,16 +10,17 @@ if %errorlevel% equ 0 (
     python manage.py loaddata data/users.json
     python manage.py loaddata data/polls-v3.json
 
-    echo Initializing environment
+    echo "Initializing environment"
     python -c "from django.core.management.utils import get_random_secret_key; f = open('.env', 'w'); f.write('SECRET_KEY=django-insecure-'+get_random_secret_key()+'\n'); f.close()"
     
-    echo DEBUG=False >> .env
-    echo ALLOWED_HOSTS=* >> .env
-    echo TIME_ZONE=Asia/Bangkok >> .env
-    echo "COMPLETED"
+    cat >> .env
+    DEBUG=False
+    ALLOWED_HOSTS=*
+    TIME_ZONE=Asia/Bangkok
+    ^D
     exit 0
 
-) else (
-    echo Error: Please install Python 3.11
-    exit 1
-)
+else
+    echo "Error: Please install Python 3.11"
+    exit 1 
+fi
